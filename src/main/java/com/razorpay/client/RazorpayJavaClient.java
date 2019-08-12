ce2821601e87bada5752803d065f8057cb8a1e03
Dhiral <dhiralkaniya3146@gmail.com>
2019-08-12 17:00:23 +0530

    Main class name changes bcz conficting with in-built RestController

---
+++ src/main/java/com/razorpay/client/RazorpayJavaClient.java
@@ -0,0 +1,11 @@
+package com.razorpay.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RazorpayJavaClient {
+    public static void main(String[] args) {
+        SpringApplication.run(RazorpayJavaClient.class, args);
+    }
+}
